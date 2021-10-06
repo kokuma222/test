2e01b7fbec27d8ef6249952e29189ec4bb3c3406
kokuma222 <ZhaoPf123@gmail.com>
2021-10-07 05:44:31 +0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def environmentTest() {
+    echo "${NEW_VERSION}"   // 双引号会解析NEW_VERSION，单引号则会原样输出
+    echo '${NEW_VERSION}'
+		echo env.GIT_BRANCH
+		echo env.BUILD_NUMBER
+		echo env.JOB_NAME
+}
+
+
+
+return this
