c81b89a4182a871d823e087e9a659183bc0281cb
kokuma222 <ZhaoPf123@gmail.com>
2021-10-07 08:01:47 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,14 @@
-def environmentTest() {
+def test() {
     echo "${NEW_VERSION}"   // 双引号会解析NEW_VERSION，单引号则会原样输出
     echo '${NEW_VERSION}'
     echo env.GIT_BRANCH
-    echo env.BUILD_NUMBER
     echo env.JOB_NAME
+    
+    echo "testing version ${params.VERSION}"
 }
 
 def toolsTest() {
     sh 'mvn --version'
-}
-
-def parametersTest() {
-    echo "testing version ${params.VERSION}"
 }
 
 return this
